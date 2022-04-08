56d4863e94cc1e5c4d7c84794d845540bf4ad2dc
Jake Garcia <j.garcia@idreamers.com>
2022-04-08 12:51:33 -0700

    Created the getDailyQuote action

---
+++ redux/actions/quoteActions/getDailyQuote.js
@@ -0,0 +1,44 @@
+
+import Urls from '../../../config/Urls';
+const {baseUrl} = Urls;
+
+const getDailyQuote = (dailyQuoteInfo) => {
+
+    const url = `${baseUrl}/quotes/get-daily-quote`;
+
+    const configuredBody = {
+        user_info: {
+            username: dailyQuoteInfo.username,
+            quote_of_the_day_date: dailyQuoteInfo.quoteOfTheDayDate
+        }
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(configuredBody)
+    };
+
+    return async dispatch => {
+        dispatch({type: "FETCHING_DAILY_QUOTE"})
+
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                let {error} = data;
+                let {hasError} = error;
+                if (hasError === true) {
+                    let {message} = error;
+                    return dispatch({type: "DAILY_QUOTE_FETCH_ERROR", errorMessage: message});
+                } else {
+                    let {quoteOfTheDay} = data;
+                    return dispatch({type: "DAILY_QUOTE_FETCH_SUCCESS", quoteOfTheDay});
+                }
+            })
+    }
+
+}
+
+export default getDailyQuote;
