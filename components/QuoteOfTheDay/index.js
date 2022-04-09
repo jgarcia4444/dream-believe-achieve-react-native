74958ff23d99699724a8423eed132d8a88cbe9d0
Jake Garcia <j.garcia@idreamers.com>
2022-04-08 20:43:46 -0700

    Added in a check to see if a quote of the day date has been set.

--- components/QuoteOfTheDay/index.js
+++ components/QuoteOfTheDay/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 
 import GlobalStyles from '../../config/GlobalStyles';
 
@@ -8,13 +9,37 @@ const {black, white, darkGray} = Colors;
 
 import QuoteCard from './QuoteCard';
 import QuoteCardActions from './QuoteCardActions';
+import getDailyQuote from '../../redux/actions/quoteActions/getDailyQuote';
 
-const QuoteOfTheDay = () => {
+const QuoteOfTheDay = ({session, getDailyQuote}) => {
+
+    const {dailyQuote, userInfo} = session;
+
+    const {username} = userInfo
+
+    const {quoteOfTheDayDate} = dailyQuote;
+
+    const handleFetchQuote = () => {
+        let dailyQuoteDate = quoteOfTheDayDate === "" ? Date() : quoteOfTheDayDate;
+        let dailyQuoteInfo = {
+            username: username,
+            quoteOfTheDayDate: dailyQuoteDate
+        };
+        getDailyQuote(dailyQuoteInfo);
+    }
 
     return (
         <View style={[styles.quoteOfTheDayContainer]}>
-            <QuoteCard />
-            <QuoteCardActions />
+            {quoteOfTheDayDate === '' ?
+                <TouchableOpacity onPress={handleFetchQuote}>
+                    <Text>Load your first daily quote.</Text>
+                </TouchableOpacity>
+            :
+            <>
+                <QuoteCard />
+                <QuoteCardActions />
+            </>
+            }
         </View>
     )
 }
@@ -28,4 +53,19 @@ const styles = StyleSheet.create({
     }
 });
 
-export default QuoteOfTheDay;
+const mapStateToProps = state => {
+    return {
+        session: state.session,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getDailyQuote: (dailyQuoteInfo) => dispatch(getDailyQuote(dailyQuoteInfo)),
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(QuoteOfTheDay);
