9a60aba4e1c2e1a5e8202a4d010c30d3379552e2
Jake Garcia <bballwiz4@gmail.com>
2022-05-17 10:30:28 -0700

    Repositioned the inputs to be flex start vertically. Added in decription text and a send code button.

--- screens/ForgotPassword/ForgotPassword.js
+++ screens/ForgotPassword/ForgotPassword.js
@@ -1,5 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Animated } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements'
+
 
 import GlobalStyles from '../../config/GlobalStyles';
 const { container } = GlobalStyles;
@@ -12,6 +14,8 @@ import FormInput from '../../shared/FormInput';
 
 const ForgotPassword = () => {
 
+    const headerHeight = useHeaderHeight();
+
     const [email, setEmail] = useState("");
     const [emailError, setEmailError] = useState("");
 
@@ -23,17 +27,40 @@ const ForgotPassword = () => {
     }
 
     return (
-        <View style={[container, styles.forgotPasswordContainer]}>
+        <View style={[container, styles.forgotPasswordContainer, {paddingTop: headerHeight + height * 0.05}]}>
             <Background />
+            <Text style={styles.descriptionText}>Enter the email address associated with your account.</Text>
             <FormInput inputObject={inputObject} />
+            <TouchableOpacity style={styles.actionButton}>
+                <Text style={styles.sendCodeText}>Send Code</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    forgotPasswordContainer: {
+const { height, width } = Dimensions.get('screen');
 
-    }
+const styles = StyleSheet.create({
+    actionButton: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 0,
+        borderColor: black,
+        borderRadius: width / 2,
+        paddingVertical: height * 0.02,
+        backgroundColor: blackOpaque,
+    },
+    descriptionText: {
+        fontSize: 16,
+    },
+    forgotPasswordContainer: {
+        justifyContent: 'flex-start',
+    },
+    sendCodeText: {
+        fontSize: 24,
+        color: white,
+    },
 });
 
 export default ForgotPassword;
